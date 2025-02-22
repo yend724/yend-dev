912a1e1615bb086751f5b5809a8d941a381ae9e0
yend724 <yend9524@gmail.com>
2025-02-22 14:24:45 +0900

    wip

--- src/entities/post/model/schema.ts
+++ src/entities/post/model/schema.ts
@@ -4,7 +4,7 @@ export const componentSchema = v.function();
 
 export const frontmatterSchema = v.object({
   title: v.string(),
-  date: v.string(),
+  date: v.pipe(v.string(), v.regex(/^\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}$/)),
   draft: v.boolean(),
   tags: v.undefinedable(v.array(v.string()), () => []),
 });
