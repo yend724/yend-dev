e9d35e7bda95115d63b5b6aba51375116adfb208
yend724 <65233817+yend724@users.noreply.github.com>
2025-02-23 12:14:48 +0900

    wip

--- src/shared/ui/header/index.tsx
+++ src/shared/ui/header/index.tsx
@@ -9,7 +9,7 @@ export const Header: React.FC<Props> = ({
   renderTitle = (title) => <>{title}</>,
 }) => {
   return (
-    <header className="mx-auto grid w-full max-w-5xl grid-cols-[auto_auto] justify-between p-4">
+    <header className="mx-auto grid w-full max-w-5xl grid-cols-[auto_auto] items-center justify-between p-4">
       <Link href="/" className="font-bold">
         {renderTitle(SITE_METADATA.title)}
       </Link>
