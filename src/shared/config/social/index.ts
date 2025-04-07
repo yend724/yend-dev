0358f2140d21a21ceccee12e4c168192637e0abb
yend724 <yend9524@gmail.com>
2025-04-07 20:20:17 +0900

    wip

--- src/shared/config/social/index.ts
+++ src/shared/config/social/index.ts
@@ -2,7 +2,7 @@ export const SOCIALS = [
   { label: "GitHub", href: "https://github.com/yend724" },
   { label: "Zenn", href: "https://zenn.dev/yend724" },
   { label: "Qiita", href: "https://qiita.com/yend724" },
-  { label: "X(旧Twitter)", href: "https://x.com/yend724" },
+  { label: "X", href: "https://x.com/yend724" },
   { label: "mixi2", href: "https://mixi.social/@yend724" },
   { label: "CodePen", href: "https://codepen.io/yend24" },
 ];
