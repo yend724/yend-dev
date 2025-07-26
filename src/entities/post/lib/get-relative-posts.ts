23abcbcc37a239d00af7637aa9bbbb15f549a0ae
yend724 <yend9524@gmail.com>
2025-07-26 11:05:45 +0900

    wip

--- src/entities/post/lib/get-relative-posts.ts
+++ src/entities/post/lib/get-relative-posts.ts
@@ -1,9 +1,15 @@
 import { Post } from "../model/post";
 
-export const getRelativePosts = (posts: Post[], slug: string, frontmatter: Post['frontmatter']) => {
-  const sameTagPosts = posts.filter((post) =>
-    post.frontmatter.tags.some((tag) => frontmatter.tags.includes(tag))
-  ).filter((post) => post.slug !== slug);
+export const getRelativePosts = (
+  posts: Post[],
+  slug: string,
+  frontmatter: Post["frontmatter"]
+) => {
+  const sameTagPosts = posts
+    .filter((post) =>
+      post.frontmatter.tags.some((tag) => frontmatter.tags.includes(tag))
+    )
+    .filter((post) => post.slug !== slug);
 
   const sortedPosts = [...sameTagPosts].sort(
     (a, b) =>
@@ -11,5 +17,5 @@ export const getRelativePosts = (posts: Post[], slug: string, frontmatter: Post[
       new Date(a.frontmatter.date).getTime()
   );
 
-  return sortedPosts
-}
+  return sortedPosts;
+};
