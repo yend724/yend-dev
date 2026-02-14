fa24fcc2d6751682e48fe47df5bbfe6dc3d9f88e
yend724 <yend9524@gmail.com>
2026-02-14 15:32:25 +0900

    wip

--- packages/v1/src/views/posts/[slug]/ui/tags/index.tsx
+++ packages/v1/src/views/posts/[slug]/ui/tags/index.tsx
@@ -5,7 +5,10 @@ export const Tags: React.FC<Props> = ({ tags }) => {
   return (
     <ul className="flex flex-wrap gap-2">
       {tags.sort().map((tag) => (
-        <li key={tag} className="bg-blue-9 rounded-sm px-1 text-sm">
+        <li
+          key={tag}
+          className="bg-blue-9 text-blue-12 rounded-sm px-1 text-sm"
+        >
           {tag}
         </li>
       ))}
