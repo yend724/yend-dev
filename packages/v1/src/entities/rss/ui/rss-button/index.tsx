18112e500235190e2730916479136397869369e4
yend724 <yend9524@gmail.com>
2026-02-28 22:23:14 +0900

    wip

--- packages/v1/src/entities/rss/ui/rss-button/index.tsx
+++ packages/v1/src/entities/rss/ui/rss-button/index.tsx
@@ -1,13 +1,12 @@
-// import { Rss as RssIcon } from "lucide-react";
+import { Rss as RssIcon } from "lucide-react";
 
-// import { SITE_METADATA } from "../../../../shared/config/site";
-// import { Link } from "../../../../shared/ui/link";
+import { SITE_METADATA } from "../../../../shared/config/site";
+import { Link } from "../../../../shared/ui/link";
 
 export const RssButton = () => {
-  return null;
-  // return (
-  //   // <Link className="hover:bg-gray-4 rounded-full p-2" href={SITE_METADATA.rss}>
-  //   //   <RssIcon size={18} aria-label="RSSを購読する" />
-  //   // </Link>
-  // );
+  return (
+    <Link className="hover:bg-gray-4 rounded-full p-2" href={SITE_METADATA.rss}>
+      <RssIcon size={18} aria-label="RSSを購読する" />
+    </Link>
+  );
 };
