9c901b7217184032638e4789937b225de548c8ea
yend724 <yend9524@gmail.com>
2026-02-28 22:10:14 +0900

    wip

--- packages/v1/src/entities/rss/ui/rss-button/index.tsx
+++ packages/v1/src/entities/rss/ui/rss-button/index.tsx
@@ -1,11 +1,11 @@
 import { Rss as RssIcon } from "lucide-react";
 
-import { SITE_PATHS } from "../../../../shared/config/path";
+import { SITE_METADATA } from "../../../../shared/config/site";
 import { Link } from "../../../../shared/ui/link";
 
 export const RssButton = () => {
   return (
-    <Link className="hover:bg-gray-4 rounded-full p-2" href={SITE_PATHS.RSS}>
+    <Link className="hover:bg-gray-4 rounded-full p-2" href={SITE_METADATA.rss}>
       <RssIcon size={18} aria-label="RSSを購読する" />
     </Link>
   );
--- packages/v1/src/shared/config/path/index.ts
+++
@@ -1,6 +0,0 @@
-export const SITE_PATHS = {
-  ROOT: "/",
-  RSS: "/rss.xml",
-};
-
-export const PROJECT_PATHS = {};
